ddc4fffd5ef29f9133e16e398b2229d0a99b7d47
theu <teuzin375@gmail.com>
2021-12-03 15:54:12 -0300

    refactor: create variable database replace firebase.data

--- public/assets/js/main-v1.js
+++ public/assets/js/main-v1.js
@@ -5,8 +5,13 @@
 
 const id = localStorage.getItem('id_aqm');
 let tempo = 0;
+const queue_ids_calls = [];
+const audio = new Audio(
+  'assets/sounds/salamisound-2028068-ding-dong-bell-doorbell.mp3',
+);
 const container = document.querySelector('#screen');
 const visor_time = document.querySelector('#visor_time');
+const database = firebase.database();
 
 firebase.auth().onAuthStateChanged(firebaseUser => {
   if (!firebaseUser) {
@@ -17,7 +22,7 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
   }
 });
 
-function createModel_QueueCalls() {
+function createModelQueueCalls() {
   const data = {
     n: 0,
     p: 0,
@@ -38,7 +43,7 @@ function createModel_QueueCalls() {
       },
     ],
   };
-  const getId = firebase.database().ref().child('queueCalls').push(data).key;
+  const getId = database.ref().child('queueCalls').push(data).key;
   localStorage.setItem('id_aqm', getId);
 }
 
@@ -70,7 +75,7 @@ function startCountdown() {
 }
 
 function clearDB(e) {
-  const dbDel = firebase.database().ref().child('calls');
+  const dbDel = database.ref().child('calls');
 
   dbDel
     .remove()
@@ -90,13 +95,13 @@ function generateRecord(e) {
           pass:
             e.target.id === 'generate_record_nm'
               ? `NM${
-                  data_updated.n > 10
-                    ? data_updated.n
+                  data_updated.n >= 10
+                    ? (data_updated.n += 1)
                     : `0${(data_updated.n += 1)}`
                 }`
               : `PD${
-                  data_updated.p > 10
-                    ? data_updated.p
+                  data_updated.p >= 10
+                    ? (data_updated.p += 1)
                     : `0${(data_updated.p += 1)}`
                 }`,
         },
@@ -111,8 +116,7 @@ function generateRecord(e) {
       if (id) {
         const updates = {};
         updates[`/queueCalls/${id}`] = data_updated;
-        firebase
-          .database()
+        database
           .ref()
           .update(updates)
           .catch(() => {
@@ -123,51 +127,86 @@ function generateRecord(e) {
 }
 
 function nextCall(e) {
-  firebase
-    .database()
-    .ref(`queueCalls/${id}`)
-    .once('value', item => {
-      const data_updated = item.val();
-      let element;
-      let data;
+  database.ref(`queueCalls/${id}`).once('value', item => {
+    const data_updated = item.val();
+    let element;
+    let data;
 
-      if (e.target.id === 'next_nm') {
-        element = data_updated.queue_nm;
-        if (!(element.length >= 2)) {
-          return alert('Sem fila');
-        }
-        data = element.splice(1, 1);
-        data_updated.queue_nm = element;
-      } else {
-        element = data_updated.queue_pd;
-        if (!(element.length >= 2)) {
-          return alert('Sem fila');
-        }
-        data = element.splice(1, 1);
-        data_updated.queue_pd = element;
+    if (e.target.id === 'next_nm') {
+      element = data_updated.queue_nm;
+      if (!(element.length >= 2)) {
+        return alert('Sem fila');
       }
-
-      data[0].client.local = `${window.location.search
-        .split('?')[1]
-        .split('l=')[1]
-        .toLocaleUpperCase()} ${
-        window.location.search.split('?')[2].split('n=')[1]
-      }`;
-
-      if (id) {
-        const updates = {};
-        updates[`/queueCalls/${id}`] = data_updated;
-        firebase
-          .database()
-          .ref()
-          .update(updates)
-          .catch(() => {
-            alert('not updated');
-          });
+      data = element.splice(1, 1);
+      data_updated.queue_nm = element;
+    } else {
+      element = data_updated.queue_pd;
+      if (!(element.length >= 2)) {
+        return alert('Sem fila');
       }
+      data = element.splice(1, 1);
+      data_updated.queue_pd = element;
+    }
 
-      return firebase.database().ref().child('calls').push(data[0]);
-    });
+    data[0].client.local = `${window.location.search
+      .split('?')[1]
+      .split('l=')[1]
+      .replace('guiche', 'GUICHÊ')
+      .toLocaleUpperCase()} ${
+      window.location.search.split('?')[2].split('n=')[1]
+    }`;
+
+    if (id) {
+      const updates = {};
+      updates[`/queueCalls/${id}`] = data_updated;
+      database
+        .ref()
+        .update(updates)
+        .catch(() => {
+          alert('not updated');
+        });
+    }
+
+    return database.ref().child('calls').push(data[0]);
+  });
+}
+
+async function updateQueue(data) {
+  const visor_nm = document.querySelector('#visor_nm');
+  const visor_pd = document.querySelector('#visor_pd');
+  tempo = 0;
+
+  if (data.val().length === 0) return;
+
+  const { client } = data.val();
+  const { local, pass } = client;
+  queue_ids_calls.push(data.key);
+
+  if (visor_nm && visor_pd) {
+    if (pass.split('PD')[1]) {
+      visor_pd.innerText = `${local} - ${pass}`;
+    }
+    visor_nm.innerText = `${local} - ${pass}`;
+  }
+
+  const create = document.createElement('div');
+  create.setAttribute('class', 'visor');
+
+  const input1 = document.createElement('input');
+  input1.setAttribute('type', 'text');
+  input1.setAttribute('disabled', 'disabled');
+  input1.setAttribute('value', local);
+
+  const input2 = document.createElement('input');
+  input2.setAttribute('type', 'text');
+  input2.setAttribute('disabled', 'disabled');
+  input2.setAttribute('value', pass);
+
+  create.appendChild(input1);
+  create.appendChild(input2);
+  container.appendChild(create);
+
+  document.getElementsByClassName('visor')[0].remove();
 }
 
 const clear_queue = document.querySelector('#clearQueue');
@@ -187,61 +226,29 @@ const generate_record_pd = document.querySelector('#generate_record_pd');
 if (generate_record_pd)
   generate_record_pd.addEventListener('click', generateRecord);
 
-const createModelQueueCalls = document.querySelector('#createModel_QueueCalls');
-if (createModelQueueCalls)
-  createModelQueueCalls.addEventListener('click', createModel_QueueCalls);
-
-function updateQueue(data) {
-  tempo = 0;
-  const visor_nm = document.querySelector('#visor_nm');
-  const visor_pd = document.querySelector('#visor_pd');
-
-  if (!container) return;
-
-  if (data.val().length === 0) return;
-
-  const audio = new Audio(
-    'assets/sounds/salamisound-2028068-ding-dong-bell-doorbell.mp3',
-  );
-  const { client } = data.val();
-  const { local, pass } = client;
-  if (visor_nm && visor_pd) {
-    if (!pass.split('PD')[1]) {
-      visor_nm.innerText = `${local} - ${pass}`;
-    }
-    visor_pd.innerText = `${local} - ${pass}`;
-  }
-
-  if (!container) return;
-  const create = document.createElement('div');
-  create.setAttribute('class', 'visor');
-
-  const input1 = document.createElement('input');
-  input1.setAttribute('type', 'text');
-  input1.setAttribute('disabled', 'disabled');
-  input1.setAttribute('value', local);
-
-  const input2 = document.createElement('input');
-  input2.setAttribute('type', 'text');
-  input2.setAttribute('disabled', 'disabled');
-  input2.setAttribute('value', pass);
-  audio.play();
-
-  create.appendChild(input1);
-  create.appendChild(input2);
-  container.appendChild(create);
-
-  document.getElementsByClassName('visor')[0].remove();
-}
+const createModel_QueueCalls = document.querySelector(
+  '#createModel_QueueCalls',
+);
+if (createModel_QueueCalls)
+  createModel_QueueCalls.addEventListener('click', createModelQueueCalls);
 
 if (container) {
-  firebase
-    .database()
-    .ref('calls')
-    .on('value', snapshot => {
-      snapshot.forEach(item => {
-        updateQueue(item);
+  database.ref('calls').on('value', snapshot => {
+    snapshot.forEach(item => {
+      updateQueue(item).then(() => {
+        audio.play();
+        if (queue_ids_calls.length >= 7) {
+          const dbDel = firebase
+            .database()
+            .ref()
+            .child(`/calls/${queue_ids_calls[0]}`);
+
+          dbDel.remove().catch(error => {
+            alert(`Remove failed: ${error.message}`);
+          });
+        }
       });
     });
+  });
   startCountdown();
 }
